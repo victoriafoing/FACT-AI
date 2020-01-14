ba602523092079183c4b18febddfb3061064b92e
Gaurav Kudva <gauravkudva2@gmail.com>
2020-01-14 14:03:11 +0100

    Word Embeddings Properly Loading

--- load_data.py
+++ load_data.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 from pathlib import Path
-from typing import List
+from typing import List, Dict
 from utility_functions import *
 import config
 
@@ -26,11 +26,11 @@ def load_data(path: Path = Path('./data/google-analogies.txt')) -> List[RawDatap
     return dataset
 
 # Function to transform the raw data into their corresponding word embeddings
-def transform_data(analogy_dataset : List[RawDatapoint]) -> List[Datapoint]:
+def transform_data(word_vectors : Dict, analogy_dataset : List[RawDatapoint]) -> List[Datapoint]:
     # List to store the transformed datapoints
     transformed_dataset = []
     # Obtaining the gender word pairs
-    gender_pairs = obtain_gender_pairs(config.embedding_data_path, config.save_dir, config.save_file)
+    gender_pairs = obtain_gender_pairs(word_vectors)
     # Obtaining the gender subspace
     gender_subspace = obtain_gender_subspace(gender_pairs)
     # For each Raw_Datapoint tuple in the list
@@ -38,8 +38,7 @@ def transform_data(analogy_dataset : List[RawDatapoint]) -> List[Datapoint]:
         # Temporary transformed datapoint
         temp_datapoint = Datapoint()
         # Obtaining a list of the corresponding word embeddings
-        embeddings = load_vectors(config.embedding_data_path, config.save_dir, config.save_file, \
-            [raw_datapoint.x1, raw_datapoint.x2, raw_datapoint.x3, raw_datapoint.y])
+        embeddings = load_vectors(word_vectors, [raw_datapoint.x1, raw_datapoint.x2, raw_datapoint.x3, raw_datapoint.y])
         # Stacking the embeddings horizontally : [1 X 3D]
         temp_datapoint.analogy_embeddings = np.hstack(embeddings[0 : 3])
         # Obtaining the embedding corresponding to y
--- utility_functions.py
+++ utility_functions.py
@@ -5,29 +5,29 @@ from sklearn.decomposition import PCA
 from load_vectors import *
 
 # Function to obtain the male-female gender word pairs
-def obtain_gender_pairs(data_path : str, save_dir : str, save_file : str) -> List[List]:
+def obtain_gender_pairs(word_vectors : Dict) -> List[List]:
     # List to store the male-female gender word pairs
     pairs = []
     # Defining the word-pairs
-    pairs.append(load_vectors(data_path, save_dir, save_file, ["he", "she"]))
-    pairs.append(load_vectors(data_path, save_dir, save_file, ["his", "her"]))
-    pairs.append(load_vectors(data_path, save_dir, save_file, ["man", "woman"]))
-    pairs.append(load_vectors(data_path, save_dir, save_file, ["himself", "herself"]))
-    pairs.append(load_vectors(data_path, save_dir, save_file, ["son", "daughter"]))
-    pairs.append(load_vectors(data_path, save_dir, save_file, ["father", "mother"]))
-    pairs.append(load_vectors(data_path, save_dir, save_file, ["guy", "gal"]))
-    pairs.append(load_vectors(data_path, save_dir, save_file, ["boy", "girl"]))
-    pairs.append(load_vectors(data_path, save_dir, save_file, ["male", "female"]))
-    pairs.append(load_vectors(data_path, save_dir, save_file, ["John", "Mary"]))
+    pairs.append(load_vectors(word_vectors, ["he", "she"]))
+    pairs.append(load_vectors(word_vectors, ["his", "her"]))
+    pairs.append(load_vectors(word_vectors, ["man", "woman"]))
+    pairs.append(load_vectors(word_vectors, ["himself", "herself"]))
+    pairs.append(load_vectors(word_vectors, ["son", "daughter"]))
+    pairs.append(load_vectors(word_vectors, ["father", "mother"]))
+    pairs.append(load_vectors(word_vectors, ["guy", "gal"]))
+    pairs.append(load_vectors(word_vectors, ["boy", "girl"]))
+    pairs.append(load_vectors(word_vectors, ["male", "female"]))
+    pairs.append(load_vectors(word_vectors, ["john", "mary"]))
     # Returning the pairs
     return pairs
 
 # Function to obtain the gender subspace or direction
-def obtain_gender_subspace(pairs : List[List], components: int = 1) -> np.ndarray:
+def obtain_gender_subspace(pairs : List[List[List]], components: int = 1) -> np.ndarray:
     # Obtaining the means of each pair
-    means = [sum(x) / len(x) for x in pairs]
+    means = [np.sum(np.array(x), axis = 0) / np.array(x).shape[0] for x in pairs]
     # Obtaining the differences
-    differences = np.array([x - means[i] for i in range(len(means)) for x in pairs[i]])
+    differences = np.array([np.array(x) - means[i] for i in range(len(means)) for x in pairs[i]])
     # Obtaining an object of the PCA class
     pca = PCA(n_components = components)
     # Fitting the PCA object onto the difference matrix
